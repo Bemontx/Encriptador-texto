f662a967e6187cf889756c3be3c24599a17c918e
Bemontx <jeisongithub@outlook.com>
2023-05-11 15:11:42 -0500

    alura

---
+++ alura.js
@@ -0,0 +1,45 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado";
+    parrafo.textContent = "";
+  } else {
+    tituloMensaje.textContent = "Ningún texto fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar";
+    swal("Debes ingresar un texto");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado";
+    parrafo.textContent = "";
+  } else {
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar";
+    swal("Debes ingresar un texto");
+  }
+}
